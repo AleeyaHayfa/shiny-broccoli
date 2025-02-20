24fa8ecce630e1f1fc5eba0dacf69ad574252d2a
eatyourbrocoli <121542712+AleeyaHayfa@users.noreply.github.com>
2025-02-20 18:54:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
 
 st.title('🤖 ThinkTankers ML App')
 
